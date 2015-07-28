27f094fca8a531c65dfe8a7ccb8202c53993b9af
omaracrystal <omaracrystal@gmail.com>
2015-07-28 15:45:57 -0600

    added 3 images and jquery code to change background images

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 	console.log("The document is ready!");
 })
+
+$(".image-container").append("<article class= 'home' data-price='80000'> <img src= 'img/lawn.jpg'><h4>$80,000</h4></article>")
+
+$(".image-container").append("<article class= 'home' data-price='10000'> <img src= 'img/old.jpg'><h4>$10,000</h4></article>")
+
+$(".image-container").append("<article class= 'home' data-price='800000'> <img src= 'img/stones.jpg'><h4>$800,000</h4></article>")
+
+$(".dark").on("click", function() {
+  $("body").css("background-image", "url('img/dark_wood.png')");
+})
+
+$(".light").on("click", function() {
+  $("body").css("background-image", "url('img/tileable_wood_texture.png')");
+})
+
